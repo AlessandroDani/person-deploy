a67d96af0f9443fc30dcf71d3fc662e4ba29f677
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-26 23:59:20 -0500

    refactor: wrap controller responses in ResponseEntity

--- src/main/java/com/example/deploy/controller/PersonController.java
+++ src/main/java/com/example/deploy/controller/PersonController.java
@@ -3,21 +3,23 @@ package com.example.deploy.controller;
 import com.example.deploy.model.Person;
 import com.example.deploy.service.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/person")
+@RequestMapping("/api/person")
 @RequiredArgsConstructor
 public class PersonController {
     private final PersonService personService;
 
     @PostMapping
-    public Person save(@RequestBody Person person) {
-        return personService.save(person);
+    public ResponseEntity<Person> save(@RequestBody Person person) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(personService.save(person));
     }
 
     @GetMapping("/{id}")
-    public Person findByIdentification(@PathVariable Long id) {
-        return personService.findByIdentification(id);
+    public ResponseEntity<Person> findByIdentification(@PathVariable Long id) {
+        return ResponseEntity.ok(personService.findByIdentification(id));
     }
 }
