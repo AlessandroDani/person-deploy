2044e4d486e2da09ee5a94879f9c32868664b268
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-26 23:53:56 -0500

    refactor: add Lombok constructors to Person entity

--- src/main/java/com/example/deploy/model/Person.java
+++ src/main/java/com/example/deploy/model/Person.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "personas")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     @Id
     private Long identification;
